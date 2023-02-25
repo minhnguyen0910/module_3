0ccdb069972e1448b490df5e7364319df6600ba7
minhnguyen0910 <minhn091099@gmail.com>
2023-02-25 10:39:15 +0700

    first commit

---
+++ ss_03/bai_1/quan_ly_sinh_vien.sql
@@ -0,0 +1,64 @@
+create database quan_ly_sinh_vien;
+use quan_ly_sinh_vien;
+create table lop_hoc(
+class_id int primary key auto_increment,
+class_name varchar(50) not null,
+class_date date not null,
+class_status int);
+create table students(
+student_id int primary key auto_increment,
+student_name varchar(50) not null,
+address varchar(50),
+phone varchar(11),
+student_status int,
+class_id int not null,
+foreign key(class_id) references  lop_hoc(class_id)
+);
+create table subjects(
+sub_id int primary key auto_increment,
+sub_name varchar(50),
+credit tinyint not null default 1,
+statuss bit default 1
+);
+create table mark(
+mark_id int primary key not null auto_increment,
+sub_id int not null,
+student_id int not null,
+mark float default 0,
+exam_time tinyint default 1,
+
+unique(sub_id,student_id),
+foreign key(sub_id) references subjects(sub_id),
+foreign key(student_id) references students(student_id)
+);
+insert into lop_hoc
+value(1,'A1','2008-12-20',1),
+(2,'A2','2008-12-22',1),
+(3,'A2',current_date(),1);
+insert into students(student_name,address,phone,student_status,class_id)
+value('Hung','Ha Noi','0912113113',1,1),
+('Hoa','Hai Phong','',1,1),
+('Manh','Hai Phong','011234323',1,1);
+insert into subjects
+value(1,'CF',5,1),
+(2,'C',6,1),
+(3,'HDJ',5,1),
+(4,'RDBMS',10,1);
+insert into mark(sub_id,student_id,mark,exam_time)
+value(1,1,8,1),
+(1,2,10,2),
+(2,1,12,1);
+select * from mark;
+select * from students
+where student_name like 'h%';
+select * from lop_hoc
+where class_date like '%-12-%';
+select * from subjects
+where credit between 3 and 5;
+update students
+set class_id=2 where student_name='Hung'; 
+select student_name,sub_name,mark
+from mark
+inner join students on mark.student_id=students.student_id
+inner join subjects on mark.sub_id=subjects.sub_id
+order by mark;
---
+++ ss_03/bai_2/quan_ly.sql
@@ -0,0 +1,36 @@
+create database quan_ly_ban_hang;
+use quan_ly_ban_hang;
+create table customer(
+c_id int primary key,
+c_name varchar(50),
+c_tuoi int
+);
+create table oder(
+o_id int primary key,
+c_id int,
+o_date date,
+o_total_price int,
+foreign key (c_id) references customer(c_id)
+);
+create table product(
+p_id int primary key,
+p_name varchar(50),
+p_price int
+);
+create table order_detail(
+p_id int,
+c_id int,
+od_qty int,
+primary key(p_id,c_id),
+foreign key (p_id) references product(p_id),
+foreign key (c_id) references oder(c_id)
+);
+insert into customer(c_id,c_name,c_tuoi)
+value(1,'Minh Quan',10),(2,'Ngoc Oanh',20),(3,'Hong Ha',50);
+insert into product(p_id,p_name,p_price)
+value(1,'May Giat',3),(2,'Tu Lanh',5),(3,'Dieu Hoa',7),
+(4,'Quat',1),(5,'Bep Dien',2);
+insert into oder(o_id,c_id,o_date,o_total_price)
+value(1,1,'2006-03-21'),(2,2,'2006-03-23'),(3,1,2006-03-16);
+insert into order_detail(p_id,c_id,od_qty)
+value(1,1,'a'),(2,2,'b');
